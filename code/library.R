88b986ed68d869526bf38bb27c2aadb5dea42b04
AkiraTerui <hanabi0111@gmail.com>
2022-11-02 17:09:20 -0400

    Create library.R

---
+++ code/library.R
@@ -0,0 +1,30 @@
+pacman::p_load(## gg
+  tidyverse,
+  patchwork,
+  ggside,
+  ggridges,
+  ggspatial,
+  cowplot,
+  ## simulation
+  cdyns,
+  foreach,
+  doParallel,
+  doSNOW,
+  ## bayes
+  runjags,
+  loo,
+  ## misc
+  forecast,
+  scales,
+  here,
+  corrplot,
+  msir,
+  stargazer,
+  knitr,
+  ## gis
+  terra,
+  raster,
+  exactextractr,
+  sf,
+  stars,
+  whitebox)
