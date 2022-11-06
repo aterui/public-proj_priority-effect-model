fbcc84f53c2ddac297d65c3e707a6052f8f81ebc
aterui <hanabi0111@gmail.com>
2022-11-06 01:43:21 -0500

    Update run_test.R

--- code/run_test.R
+++ code/run_test.R
@@ -39,84 +39,96 @@ df_para <- expand.grid(n_species = c(2, 5),
                        r = log(10),
                        alpha = c(1, 0.1),
                        k = 100,
-                       sd_env = 0.1)
+                       sd_env = 0.1) %>% 
+  mutate(i = row_number())
 
-df_out <- foreach(i = seq_len(nrow(df_para)),
+n_rep <- 5
+
+df_out <- foreach(x = iterators::iter(df_para, by = "row"),
                   .combine = bind_rows) %do% {
-  
-  x <- df_para[i, ] 
-  
-  set.seed(1)
-  list_dyn <- cdyns::cdynsim(n_species = x$n_species,
-                             n_timestep = x$n_timestep,
-                             r_type = "constant",
-                             r = x$r,
-                             int_type = "constant",
-                             alpha = x$alpha,
-                             k = x$k, 
-                             sd_env = x$sd_env,
-                             model = "bh")
-  
-  df0 <- list_dyn$df_dyn %>% 
-    mutate(count = rpois(nrow(.), lambda = density))
-  
-  df0 %>% 
-    group_by(species) %>% 
-    summarize(n_obs = sum(count > 0))
-  
-  df0 %>%
-    ggplot(aes(x = timestep,
-               y = count,
-               color = factor(species))) +
-    geom_line() +
-    geom_point()
-  
-  ## data for jags ####
-  d_jags <- list(N = df0$count,
-                 Year = df0$timestep,
-                 Species = df0$species,
-                 Nsample = nrow(df0),
-                 Nyr1 = 1,
-                 Nyr = max(df0$timestep),
-                 Nsp = n_distinct(df0$species),
-                 W = diag(n_distinct(df0$species)),
-                 Q = 1)
-  
-  ## run jags ####
-  post <- run.jags(m$model,
-                   monitor = para,
-                   data = d_jags,
-                   n.chains = n_chain,
-                   inits = inits,
-                   method = "parallel",
-                   burnin = n_burn,
-                   sample = n_sample,
-                   adapt = n_ad,
-                   thin = n_thin,
-                   n.sims = 4,
-                   module = "glm")
-  
-  mcmc_summary <- MCMCvis::MCMCsummary(post$mcmc)
-  print(max(mcmc_summary$Rhat, na.rm = T))
-  
-  while(max(mcmc_summary$Rhat, na.rm = T) >= 1.1) {
-    post <- suppressMessages(runjags::extend.jags(post,
-                                                  burnin = 0,
-                                                  sample = n_sample,
-                                                  adapt = n_ad,
-                                                  thin = n_thin,
-                                                  n.sims = n_chain,
-                                                  combine = TRUE,
-                                                  silent.jags = TRUE))
-
-    mcmc_summary <- MCMCvis::MCMCsummary(post$mcmc)
-    print(max(mcmc_summary$Rhat, na.rm = T))
-  }
-  
-  x %>% 
-    mutate(p = mcmc_summary[1, "50%"]) %>% 
-    return()
-}
+                    
+                    print(paste("i = ", x$i))
+                    
+                    foreach(j = 1:n_rep,
+                            .combine = bind_rows) %do% {
+                              
+                              print(paste("j = ", j))
+                              set.seed(j)
+                              
+                              list_dyn <- cdyns::cdynsim(n_species = x$n_species,
+                                                         n_timestep = x$n_timestep,
+                                                         r_type = "constant",
+                                                         r = x$r,
+                                                         int_type = "constant",
+                                                         alpha = x$alpha,
+                                                         k = x$k, 
+                                                         sd_env = x$sd_env,
+                                                         model = "bh")
+                              
+                              df0 <- list_dyn$df_dyn %>% 
+                                mutate(count = rpois(nrow(.), lambda = density))
+                              
+                              df0 %>% 
+                                group_by(species) %>% 
+                                summarize(n_obs = sum(count > 0))
+                              
+                              df0 %>%
+                                ggplot(aes(x = timestep,
+                                           y = count,
+                                           color = factor(species))) +
+                                geom_line() +
+                                geom_point()
+                              
+                              ## data for jags ####
+                              d_jags <- list(N = df0$count,
+                                             Year = df0$timestep,
+                                             Species = df0$species,
+                                             Nsample = nrow(df0),
+                                             Nyr1 = 1,
+                                             Nyr = max(df0$timestep),
+                                             Nsp = n_distinct(df0$species),
+                                             W = diag(n_distinct(df0$species)),
+                                             Q = 1)
+                              
+                              ## run jags ####
+                              post <- run.jags(m$model,
+                                               monitor = para,
+                                               data = d_jags,
+                                               n.chains = n_chain,
+                                               inits = inits,
+                                               method = "parallel",
+                                               burnin = n_burn,
+                                               sample = n_sample,
+                                               adapt = n_ad,
+                                               thin = n_thin,
+                                               n.sims = 4,
+                                               module = "glm")
+                              
+                              mcmc_summary <- MCMCvis::MCMCsummary(post$mcmc)
+                              print(max(mcmc_summary$Rhat, na.rm = T))
+                              
+                              while(max(mcmc_summary$Rhat, na.rm = T) >= 1.1) {
+                                post <- suppressMessages(runjags::extend.jags(post,
+                                                                              burnin = 0,
+                                                                              sample = n_sample,
+                                                                              adapt = n_ad,
+                                                                              thin = n_thin,
+                                                                              n.sims = n_chain,
+                                                                              combine = TRUE,
+                                                                              silent.jags = TRUE))
+                                
+                                mcmc_summary <- MCMCvis::MCMCsummary(post$mcmc)
+                                print(max(mcmc_summary$Rhat, na.rm = T))
+                              }
+                              
+                              x %>% 
+                                mutate(n_rep = j,
+                                       p = mcmc_summary[1, "50%"]) %>% 
+                                relocate(n_rep) %>% 
+                                return()
+                              
+                            }
+                  }
 
 
 # plot --------------------------------------------------------------------
@@ -135,6 +147,6 @@ df_out %>%
        x = "Timestep") +
   theme_bw()
 
-ggsave(filename = "output/test_low.pdf",
+ggsave(filename = "output/test.pdf",
        height = 5,
        width = 12)
