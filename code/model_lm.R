fbe040ab7ed044bdd31a2d25b560d14d48f924a7
aterui <hanabi0111@gmail.com>
2022-12-13 02:11:49 -0500

    edit

--- code/model_lm.R
+++ code/model_lm.R
@@ -5,12 +5,15 @@ model {
   
   # liklihood ---------------------------------------------------------------
   for (i in 1:Nsample) {
+    lh[i] <- log(dnorm(Y[i], mu[Year[i], Species[i]], tau))
     Y[i] ~ dnorm(mu[Year[i], Species[i]], tau)
   }
   
   for (t in 1:Nyear) {
     for (j in 1:Nsp) {
-      mu[t, j] <- b[j, 1] + b[j, 2] * scl_x_t[t, j]
+      mu[t, j] <- Z * mu1[t, j] + (1 - Z) * mu0[t, j]
+      mu0[t, j] <- b0[j, 1] + b0[j, 2] * x_t[t]
+      mu1[t, j] <- b1[j, 1] + b1[j, 2] * x_t[t]
     }
   }
   
@@ -18,28 +21,36 @@ model {
   s <- rep(2.5, K)
   
   for (j in 1:Nsp) {
-    b[j, 1:K] ~ dmnorm(mu_b[], TAU[,])
+    # model H0
+    b0[j, 1] ~ dnorm(mu_b0[1], tau_b0[1])
+    b0[j, 2] <- mu_b0[2]
+    
+    # model H1
+    b1[j, 1:K] ~ dmnorm(mu_b1[], TAU[,])
   }
   
   TAU[1:K, 1:K] ~ dscaled.wishart(s, 2)
   SIGMA[1:K, 1:K] <- inverse(TAU[,])
-  tau ~ dscaled.gamma(2.5, 3)
+  tau ~ dscaled.gamma(scale0, df0)
+  sigma <- sqrt(1 / tau)
   
   for (k in 1:K) {
-    mu_b[k] ~ dnorm(0, pow(2.5, -2))
-    tau_b[k] <- TAU[k, k]
-    sigma_b[k] <- sqrt(1/tau_b[k])
-    cv_b[k] <- sigma_b[k] / abs(mu_b[k])
+    # model H0
+    mu_b0[k] ~ dnorm(0, pow(scale0, -2))
+    tau_b0[k] ~ dscaled.gamma(scale0, df0)
+    sigma_b0[k] <- sqrt(1 / tau_b0[k])
+    
+    # model H1
+    mu_b1[k] ~ dnorm(0, pow(scale0, -2))
+    tau_b1[k] <- TAU[k, k]
+    sigma_b1[k] <- sqrt(tau_b1[k])
   }
 }
 
 data {
   
   for (t in 1:Nyear) {
-    for(j in 1:Nsp) {
-      scl_x_t[t, j] <- x_t[t, j] / sd(x_t[, j])
-      x_t[t, j] <- sum(x[t, ])
-    }
+    x_t[t] <- sum(x[t, ])
   }
   
   for (i in 1:Nsample) {
--- code/run_model_lm.R
+++ code/run_model_lm.R
@@ -9,7 +9,8 @@ source(here::here("code/library.R"))
 
 #set.seed(1)
 nsp <- 30
-r0 <- runif(nsp, 0.5, 2.5)
+r_min <- 0.5
+r_max <- 2.5
 k <- 100
 A <- matrix(runif(nsp * nsp, 1, 1),
             nsp,
@@ -17,19 +18,13 @@ A <- matrix(runif(nsp * nsp, 1, 1),
 
 diag(A) <- 1
 
-df_a <- as_tibble(c(A)) %>% 
-  bind_cols(which(!is.na(A), arr.ind = T)) %>% 
-  rename(alpha_prime = value) %>% 
-  mutate(alpha = alpha_prime * r0 / k)
-
-
 # data --------------------------------------------------------------------
 
 #set.seed(1)
 list_dyn <- cdyns::cdynsim(n_species = nsp,
                            n_timestep = 30,
                            r_type = "constant",
-                           r = r0,
+                           r = runif(nsp, r_min, r_max),
                            int_type = "manual",
                            alpha = A,
                            k = k, 
@@ -65,66 +60,76 @@ df_jags <- df_jags %>%
 ## mcmc setup ####
 n_ad <- 100
 n_iter <- 1.0E+4
-n_thin <- max(3, ceiling(n_iter / 250))
+n_thin <- max(3, ceiling(n_iter / 500))
 n_burn <- ceiling(max(10, n_iter/2))
 n_chain <- 4
 n_sample <- ceiling(n_iter / n_thin)
 
 inits <- replicate(n_chain,
                    list(.RNG.name = "base::Mersenne-Twister",
-                        .RNG.seed = NA,
-                        mu_b = c(1, 1)),
+                        .RNG.seed = NA),
                    simplify = FALSE)
 
 for (j in 1:n_chain) inits[[j]]$.RNG.seed <- (j - 1) * 10 + 1
 
 ## parameters ####
-para <- c("mu_b",
-          "sigma_b",
-          "cv_b",
-          "tau",
-          "SIGMA")
+para <- c("mu_b0",
+          "mu_b1",
+          "b0",
+          "b1",
+          "sigma",
+          "sigma_b0",
+          "sigma_b1",
+          "lh")
 
 # jags --------------------------------------------------------------------
 
-d_jags <- list(Y = df_jags$log_r,
-               X = df_jags$count0,
-               Year = df_jags$t,
-               Species = df_jags$species,
-               Nsample = nrow(df_jags),
-               Nsp = n_distinct(df_jags$species),
-               Nyear = n_distinct(df_jags$t),
-               K = 2)
+list_waic <- foreach(Z = c(0, 1)) %do% {
+  
+  d_jags <- list(Y = df_jags$log_r,
+                 X = df_jags$count0,
+                 Year = df_jags$t,
+                 Species = df_jags$species,
+                 Nsample = nrow(df_jags),
+                 Nsp = n_distinct(df_jags$species),
+                 Nyear = n_distinct(df_jags$t),
+                 K = 2,
+                 Z = Z)
+  
+  m <- read.jagsfile("code/model_lm.R")
+  post <- suppressMessages(run.jags(m$model,
+                                    monitor = para,
+                                    data = d_jags,
+                                    n.chains = n_chain,
+                                    inits = inits,
+                                    method = "parallel",
+                                    burnin = n_burn,
+                                    sample = n_sample,
+                                    adapt = n_ad,
+                                    thin = n_thin,
+                                    n.sims = 4,
+                                    module = "glm"))
+  
+  (rhat <- max(MCMCvis::MCMCsummary(post$mcmc)[,"Rhat"],
+               na.rm = T))
+  
+  while(rhat >= 1.1) {
+    post <- extend.jags(post,
+                        combine = TRUE,
+                        sample = n_sample,
+                        thin = n_thin)
+    
+    (rhat <- max(MCMCvis::MCMCsummary(post$mcmc)[,"Rhat"],
+                 na.rm = T))
+  }
+  
+  MCMCvis::MCMCchains(post$mcmc) %>% 
+    as_tibble() %>% 
+    dplyr::select(starts_with("lh")) %>% 
+    data.matrix() %>% 
+    loo::waic() %>% 
+    return()
+  
+}
 
-## run jags ####
-m <- read.jagsfile("code/model_lm.R")
-post <- suppressMessages(run.jags(m$model,
-                                  monitor = para,
-                                  data = d_jags,
-                                  n.chains = n_chain,
-                                  inits = inits,
-                                  method = "parallel",
-                                  burnin = n_burn,
-                                  sample = n_sample,
-                                  adapt = n_ad,
-                                  thin = n_thin,
-                                  n.sims = 4,
-                                  module = "glm"))
-
-(mcmc_summary <- MCMCvis::MCMCsummary(post$mcmc))
-print(max(mcmc_summary$Rhat, na.rm = T))
-
-# plot --------------------------------------------------------------------
-
-# df_jags %>%
-#   ggplot(aes(x = n_t,
-#              y = log_r,
-#              color = factor(species))) +
-#   geom_point() +
-#   geom_smooth(method = "lm",
-#               se = F)
-
-print(A)
-mcmc_summary %>%
-  as_tibble(rownames = "param") %>%
-  filter(str_detect(param, "cv"))
+loo_compare(list_waic[[1]], list_waic[[2]])
