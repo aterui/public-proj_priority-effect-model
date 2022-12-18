5c838634714ac8ae7e92f2297402b18b46b76858
AkiraTerui <hanabi0111@gmail.com>
2022-12-18 08:19:45 -0500

    edit

--- code/analysis_lm.R
+++ code/analysis_lm.R
@@ -24,58 +24,51 @@ diag(A) <- 1
 
 # simulate ----------------------------------------------------------------
 
-df_est <- foreach(i = 1:100, .combine = c) %do% {
-  
-  print(i)
-  
-  list_dyn <- cdyns::cdynsim(n_species = nsp,
-                             n_timestep = 20,
-                             r_type = "constant",
-                             r = v_r,
-                             int_type = "manual",
-                             alpha = A,
-                             k = k, 
-                             sd_env = 0.1,
-                             model = "ricker",
-                             immigration = 0)
-  
-  df0 <- list_dyn$df_dyn %>% 
-    mutate(count = rpois(nrow(.), lambda = density))
-  
-  df_jags <- df0 %>% 
-    group_by(species) %>%
-    summarize(index = all(count > 0)) %>%
-    right_join(df0, by = "species") %>%
-    ungroup() %>%
-    filter(index == TRUE) %>%
-    group_by(species) %>%
-    mutate(count0 = lag(count),
-           log_r = log(count) - log(count0)) %>%
-    drop_na(log_r) %>% 
-    mutate(t = timestep - 1) %>% 
-    ungroup() %>% 
-    group_by(timestep) %>% 
-    mutate(x = sum(count0)) %>% 
-    ungroup()
-  
-  #A <- A[unique(df_jags$species), unique(df_jags$species)]
-  
-  # analysis ----------------------------------------------------------------
-  
-  df_jags <- df_jags %>% 
-    mutate(species = as.numeric(factor(species)),
-           spf = factor(species)) %>% 
-    group_by(species) %>% 
-    mutate(scl_x = scale(x),
-           scl_x0 = scale(count0)) %>% 
-    ungroup()
-  
-  fit1 <- lm(log_r ~ spf * x, df_jags)
-  fit2 <- lm(log_r ~ spf + x, df_jags)
-  
-  bf <- AIC(fit1) - AIC(fit2)
-  
-  return(bf)
-}
+print(i)
 
-plot(density(df_est))
+list_dyn <- cdyns::cdynsim(n_species = nsp,
+                           n_timestep = 20,
+                           r_type = "constant",
+                           r = v_r,
+                           int_type = "manual",
+                           alpha = A,
+                           k = k, 
+                           sd_env = 0.1,
+                           model = "ricker",
+                           immigration = 0)
+
+df0 <- list_dyn$df_dyn %>% 
+  mutate(count = rpois(nrow(.), lambda = density))
+
+df_jags <- df0 %>% 
+  group_by(species) %>%
+  summarize(index = all(count > 0)) %>%
+  right_join(df0, by = "species") %>%
+  ungroup() %>%
+  filter(index == TRUE) %>%
+  group_by(species) %>%
+  mutate(count0 = lag(count),
+         log_r = log(count) - log(count0)) %>%
+  drop_na(log_r) %>% 
+  mutate(t = timestep - 1) %>% 
+  ungroup() %>% 
+  group_by(timestep) %>% 
+  mutate(x = sum(count0)) %>% 
+  ungroup()
+
+#A <- A[unique(df_jags$species), unique(df_jags$species)]
+
+# analysis ----------------------------------------------------------------
+
+df_jags <- df_jags %>% 
+  mutate(species = as.numeric(factor(species)),
+         spf = factor(species)) %>% 
+  group_by(species) %>% 
+  mutate(scl_x = scale(x),
+         scl_x0 = scale(count0)) %>% 
+  ungroup()
+
+fit1 <- lm(log_r ~ spf * x, df_jags)
+fit2 <- lm(log_r ~ spf + x, df_jags)
+
+bf <- AIC(fit1) - AIC(fit2)
